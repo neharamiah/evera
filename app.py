ab3d40fcee09d33554476c6f114cc13f50f570e4
neharamiah <neharamiah2006@gmail.com>
2025-04-05 14:17:41 +0530

    initial flask app setup with user model and db config

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import json
+from os import path
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+app.secret_key = 'ndsn'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'signin'
+login_manager.init_app(app)
+
+#user model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(35), unique=True, nullable=False)
+    email = db.Column(db.String(35), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+def create_database(app):
+    if not path.exists('users.db'):
+        with app.app_context():
+            db.create_all()
+            print("Created Database!")
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
+    create_database(app)
     app.run(debug=True)
