0291ee08f3a82ae74cbf8e4273b7636a1adf56d2
nuhuh <neharamiah2006@gmail.com>
2025-12-23 00:03:43 +0530

    final plstrust

--- app.py
+++ app.py
@@ -499,6 +499,8 @@ def download_report():
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_database(app)
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
